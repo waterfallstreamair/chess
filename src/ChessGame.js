d4d97fcc400d0f7c881cf4651db739d81daefa03
waterfallstreamair <waterfallstreamair@gmail.com>
2020-01-14 17:14:25 +0000

    chess first commit

---
+++ src/ChessGame.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Page from './Page';
+import './index.css'
+
+class ChessGame extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Page />
+      </div>
+    );
+  }
+}
+
+export default ChessGame;
---
+++ src/Page.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import Chess from 'chess.js'; 
+import Chessboard from 'chessboardjsx';
+
+import './index.css'
+
+class Page extends Component {
+
+  state = { fen: 'start' };
+
+  componentDidMount() {
+    this.game = new Chess();
+  }
+
+  randomMove = () => {
+    let moves = this.game.moves();
+    if (
+      this.game.game_over() === true ||
+      this.game.in_draw() === true ||
+      moves.length === 0
+    ) {
+      return;
+    }
+    const index = Math.floor(Math.random() * moves.length);
+    this.game.move(moves[index]);
+    this.setState({ fen: this.game.fen() });
+  };
+  
+  step = ({ sourceSquare, targetSquare, piece }) => {
+    if (this.game.move(`${sourceSquare}${targetSquare}`, { sloppy: true })) {
+      this.setState({ fen: this.game.fen() });
+      setTimeout(this.randomMove, 1000)
+    }
+  }
+  
+  reset = () => {
+    this.game.reset()
+    this.setState({ fen: 'start' }) 
+  }
+
+  render() {
+    const { fen } = this.state;
+    return (
+      <div>
+        {this.game && this.game.game_over() && <h1>Game Over</h1>}
+        {this.game && this.game.in_checkmate() && <h2>Checkmate</h2>}
+        {this.game &&this.game.in_stalemate() && <h2>Stalemate</h2>}
+        <Chessboard
+          width={400}
+          id="chess"
+          onDrop={this.step}
+          position={fen}
+          transitionDuration={400}
+          boardStyle={{
+            borderRadius: '7px',
+            boxShadow: `0 7px 17px rgba(0, 0, 0, 0.4)`
+          }}
+        />
+        <h3 className="reload" onClick={this.reset}>Reload Game</h3>
+      </div>)
+  }
+}
+
+export default Page
